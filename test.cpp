4918fce64712844adb773c7d1686946900d43f9a
ye8722 <ye8722@gmail.com>
2026-03-10 10:26:47 +0900

    test

--- test.cpp
+++ test.cpp
@@ -1,1 +1,24 @@
-abcde
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct human{
+    int age;
+    string name;
+
+    int getage(){ return age; }
+    string getname(){ return name; }
+};
+
+int main(){
+    human Kim, Park;
+    string tmp;
+
+    Kim.age = 10;
+    Kim.name = "yechan";
+
+    tmp = Kim.getname();
+    cout << tmp << endl;
+    return 0;
+}
